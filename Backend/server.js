fe65b3a02f47e90e304d87fb924e86d945fab02e
Jacob Eichler <jeichler7@gmail.com>
2022-09-08 16:10:26 -0600

    set up rollbar and a couple other things

--- Backend/server.js
+++ Backend/server.js
@@ -2,28 +2,44 @@ const express = require('express')
 
 const path = require('path')
 
+require('dotenv').config()
+
 const cors = require('cors')
 
 const app = express()
 
 const {createArchive, getAllArchives, deleteArchive} = require('./controller.js')
 
+const {ROLLBARTOKEN} = process.env
+
 app.use(express.json())
 app.use(cors())
 
 
+// include and initialize the rollbar library with your access token
+const Rollbar = require('rollbar')
+const rollbar = new Rollbar({
+  accessToken: ROLLBARTOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "./Client/index.html"))
+    res.sendFile(path.join(__dirname, "../Client/index.html"))
+    rollbar.log("Someone is using your page")
 })
 
 app.get('/css', (req, res) => {
-    res.sendFile(path.join(__dirname, "./Client/styles.css"))
+    res.sendFile(path.join(__dirname, "../Client/styles.css"))
 })
 
 app.get('/js', (req, res) => {
-    res.sendFile(path.join(__dirname, "./Client/frontEnd.js"))
+    res.sendFile(path.join(__dirname, "../Client/frontEnd.js"))
 })
-
 
 app.post('/api/archives', createArchive)
 
@@ -31,4 +47,6 @@ app.get('/api/archives', getAllArchives)
 
 app.delete('/api/archives/:id', deleteArchive)
 
-app.listen(4077, () => console.log('Running on port 4077'))
+const port = process.env.PORT || 4077
+
+app.listen(port, () => console.log(`Server listening on ${port}`))
